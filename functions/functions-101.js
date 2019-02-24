1d826b9d63c43688e97095ee5f14df334ca83812
thehao <chungthehao@gmail.com>
2019-02-24 12:41:55 +0700

    Functions 101

---
+++ functions/functions-101.js
@@ -0,0 +1,20 @@
+// Function - input (argument), code, output (return value)
+
+let greetUser = function () {
+    console.log('Welcome user!');
+};
+
+// greetUser();
+// greetUser();
+// greetUser();
+
+let square = function (num) {
+    let result = num * num;
+    return result;
+};
+
+let value = square(3);
+let otherValue = square(10);
+//console.log(value, otherValue);
+
+// Challenge Area
