a6c8d142f62228567271aa1454238460674a2dd4
thehao <chungthehao@gmail.com>
2019-03-05 23:28:25 +0700

    Array basics

---
+++ arrays/notes.js
@@ -0,0 +1,6 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+console.log(notes.length)
+console.log(notes[0])
+console.log(notes[notes.length - 1]) // the last item
+console.log(notes[9999999999999]) // undefined
---
+++ arrays/todo.js
@@ -0,0 +1,6 @@
+const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
+const leng = todos.length
+
+console.log(`You have ${leng} todos.`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[leng - 2]}`)
