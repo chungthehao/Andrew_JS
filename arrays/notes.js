f66b1d7a53ebce0bda9142fb094a54bf6be57dd5
thehao <chungthehao@gmail.com>
2019-03-06 23:29:34 +0700

    Manipulating Arrays with Methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,6 +1,23 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
-console.log(notes.length)
-console.log(notes[0])
-console.log(notes[notes.length - 1]) // the last item
-console.log(notes[9999999999999]) // undefined
+/* from the end */
+// console.log(notes.pop()) // remove item and return value
+// notes.push('My new note') // add item
+
+/* from the start */
+// console.log(notes.shift()) // remove item and return value
+// notes.unshift('My first note') // add item
+
+/* in middle */
+// notes.splice(1, 1) // start ở index 1, remove 1 item 
+// notes.splice(1, 0, 'This is the new second item') // add new item in index 1
+// notes.splice(1, 1, 'This is the new second item') // replace the second
+
+/* use bracket notation */
+notes[2] = 'This is now the new note 3' // replace the third
+
+console.log(notes)
+// console.log(notes.length)
+// console.log(notes[0])
+// console.log(notes[notes.length - 1]) // the last item
+// console.log(notes[9999999999999]) // undefined
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,6 +1,14 @@
 const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
 const leng = todos.length
 
+// Delete the 3rd item
+todos.splice(2, 1)
+// Add a new item onto the end
+todos.push('Learn javascript')
+// Remove the first item from the list
+todos.shift()
+
 console.log(`You have ${leng} todos.`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[leng - 2]}`)
+console.log(todos)
+// console.log(`Todo: ${todos[0]}`)
+// console.log(`Todo: ${todos[leng - 2]}`)
