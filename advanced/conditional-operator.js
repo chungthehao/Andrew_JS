17387988d71426f490fc7afa6759658f1768ae6c
Chung Thế Hào <chungthehao@gmail.com>
2019-04-15 11:27:09 +0700

    Conditional (Ternary) Operator

---
+++ advanced/conditional-operator.js
@@ -0,0 +1,30 @@
+const team = ['Tyler', 'Porter', 'Henry', 'John', 'Brad']
+const message = team.length <= 4 ? `Team size: ${team.length}` : 'Too many people on your team'
+console.log(message)
+
+
+
+// const myAge = 20
+// const showPage = () => {
+//     return 'Showing the page.'
+// }
+// const showErrorPage = () => {
+//     return 'Showing the error page.'
+// }
+
+// const message = myAge >= 21 ? showPage() : showErrorPage()
+// console.log(message)
+
+
+// const myAge = 27
+// const message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
+// console.log(message)
+
+// let message
+// message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
+
+// if (myAge >= 18) {
+//     message = 'You can vote!'
+// } else {
+//     message = 'You cannot vote.'
+// }
