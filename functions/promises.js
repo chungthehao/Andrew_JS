38cb08f513a0dc7307ff283736c0d0227e2cb6cc
Chung Thế Hào <chungthehao@gmail.com>
2019-04-21 20:14:28 +0700

    Promise - Closure, truyen tham so

--- functions/promises.js
+++ functions/promises.js
@@ -24,24 +24,22 @@ getDataCallback((error, data) => {
 
 console.log(3)
 
+
 /**
- * * PROMISE
+ * * PROMISE (Mà muốn truyền params [closures])
  * - param truyền vào Promise constructor function là 1 function, function này được chạy ngay lập tức, thực hiện các việc 'tốn thời gian' (This is where we put our long running process)
  */
-const myPromise = new Promise((resolve, reject) => {
-    console.log(111)
+const getDataPromise = (thamSo) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`This is my success data: ${thamSo}`)
+            // reject('This is the PROMISE error')
+        }, 2000)
+    })
+}
 
-    setTimeout(() => {
-        // resolve('This is the PROMISE data')
+const myPromise = getDataPromise('Tham số của mình đưa vào')
 
-        reject('This is the PROMISE error') // chỉ reject hoặc resolve 1 lần duy nhất
-        reject('This is the PROMISE error')
-    }, 2000)
-
-    console.log(222)
-})
-
-// - 'then' định nghĩa what we want to do when we actually have the information. Hàm trong then được chạy khi promise resolve
 myPromise.then(
     (data) => {
         console.log(data)
@@ -51,13 +49,42 @@ myPromise.then(
     }
 )
 
-myPromise.then(
-    (data) => {
-        console.log('Làm gì đó khác với cùng 1 dữ liệu trả về')
-    },
-    (err) => {
-        console.log('Đây là error')
-    }
-)
 
-console.log(333)
+/**
+ * * PROMISE
+ * - param truyền vào Promise constructor function là 1 function, function này được chạy ngay lập tức, thực hiện các việc 'tốn thời gian' (This is where we put our long running process)
+ */
+// const myPromise = new Promise((resolve, reject) => {
+//     console.log(111)
+
+//     setTimeout(() => {
+//         // resolve('This is the PROMISE data')
+
+//         reject('This is the PROMISE error') // chỉ reject hoặc resolve 1 lần duy nhất
+//         reject('This is the PROMISE error')
+//     }, 2000)
+
+//     console.log(222)
+// })
+
+// ** 'then' định nghĩa what we want to do when we actually have the information. Hàm trong then được chạy khi promise resolve
+// myPromise.then(
+//     (data) => {
+//         console.log(data)
+//     },
+//     (err) => {
+//         console.log(err)
+//     }
+// )
+
+// ** then 2 lần nhưng chỉ đợi 2 giây thôi, thực thi 1 lần dùng dữ liệu đó chạy 2 lần
+// myPromise.then(
+//     (data) => {
+//         console.log('Làm gì đó khác với cùng 1 dữ liệu trả về')
+//     },
+//     (err) => {
+//         console.log('Đây là error')
+//     }
+// )
+
+// console.log(333)
